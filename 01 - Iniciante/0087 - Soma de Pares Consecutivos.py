a7cb912b94bfdcde7d6e352816eacceb3de55d61
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2020-12-10 01:27:28 -0300

    URI ONLINE JUDGE

--- 01 - Iniciante/0087 - Soma de Pares Consecutivos.py
+++ 01 - Iniciante/0087 - Soma de Pares Consecutivos.py
@@ -1,4 +1,13 @@
 x = 1
+y = 0
 soma = 0
 while x != 0:
-    pass
+    x = int(input())
+    while y != 5:
+        if x % 2 == 0:
+            soma += x
+            y += 1
+        x += 1
+    print(soma)
+    y = 0
+    soma = 0
