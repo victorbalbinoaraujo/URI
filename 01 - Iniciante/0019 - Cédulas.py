1a3d580b271cccff770a5c26fbabef6f4ea28751
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-01-18 10:16:13 -0300

    URI ONLINE JUDGE

--- 01 - Iniciante/0019 - Cédulas.py
+++ 01 - Iniciante/0019 - Cédulas.py
@@ -1,21 +1,14 @@
 dinheiro = int(input())
-
-print(f'{int(dinheiro / 100)} notas de R$ 100.00')
+print(f'{dinheiro / 100} notas de R$ 100.00')
 aux = dinheiro % 100
-
-print(f'{int(aux / 50)} notas de R$ 50.00')
+print(f'{aux / 50} notas de R$ 50.00')
 aux %= 50
-
-print(f'{int(aux / 20)} notas de R$ 20.00')
+print(f'{aux / 20} notas de R$ 20.00')
 aux %= 20
-
-print(f'{int(aux / 10)} notas de R$ 10.00')
+print(f'{aux / 10} notas de R$ 10.00')
 aux %= 10
-
-print(f'{int(aux / 5)} notas de R$ 5.00')
+print(f'{aux / 5} notas de R$ 5.00')
 aux %= 5
-
-print(f'{int(aux / 2)} notas de R$ 2.00')
+print(f'{aux / 2} notas de R$ 2.00')
 aux %= 2
-
-print(f'{int(aux / 1)} notas de R$ 1.00')
+print(f'{aux / 1} notas de R$ 1.00')
