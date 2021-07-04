6663356d5986fa75d929719dfc19a6d6411f26dd
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-07-04 18:25:03 -0300

    Exercício 0107

--- 01 - Iniciante/0107 - Área Esquerda.py
+++ 01 - Iniciante/0107 - Área Esquerda.py
@@ -3,22 +3,31 @@ import random
 matriz = []
 tam = 12
 soma = 0
-t = str(input()).upper()
+
+o = str(input()).upper()
+
 for i in range(tam):
     vaux = []
     for j in range(tam):
         vaux.append(round(random.uniform(-100.0, 100.0), 2))
     matriz.append(vaux)
-
-for i in range(1, 10):
+    
+# 1 e 10 extremos base Δ
+for i in range(1, 11):
+    # Até o 5, a área verde é i > j
     if i <= 5:
-        for j in range(j < i):
+        j = 0
+        while i > j:
             soma += matriz[i][j]
+            j += 1
     else:
-        for j in range(11 - i):
+        j = 0
+        # 6,0..4 7,0..3 8,0..2 9,0..1, 10,0
+        while i + j <= 10:
             soma += matriz[i][j]
+            j += 1
 
-if t == 'S':
+if o == 'S':
     print(soma) 
-elif t == 'M':
+elif o == 'M':
     print(soma / tam)
