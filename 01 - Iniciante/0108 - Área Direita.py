ca4fa288e4fe75d4495597b1ff256f937f06cb09
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-07-04 18:42:12 -0300

    Exercício 0108

---
+++ 01 - Iniciante/0108 - Área Direita.py
@@ -0,0 +1,30 @@
+import random
+
+matriz = []
+tam = 12
+soma = 0
+
+o = str(input()).upper()
+
+for i in range(tam):
+    vaux = []
+    for j in range(tam):
+        vaux.append(round(random.uniform(-100.0, 100.0), 2))
+    matriz.append(vaux)
+
+for i in range(1, 11):
+    if i <= 5:
+        j = 11
+        while i + j >= 12:
+            soma += matriz[i][j]
+            j -= 1
+    else:
+        j = 11
+        while j > i:
+            soma += matriz[i][j]
+            j -= 1
+
+if o == 'S':
+    print(soma) 
+elif o == 'M':
+    print(soma / tam)
